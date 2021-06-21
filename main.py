a768dcd578b49de6d2fa7d919706a2a6daf2bf3b
Junsu Lim <blockboy2957@gmail.com>
2021-06-21 19:16:56 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import re
 from pathlib import Path
 
 import pandas as pd
-# import random
 import time
 from bs4 import BeautifulSoup
 from nltk.corpus import stopwords
@@ -14,10 +13,6 @@ from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from func_timeout import func_timeout, FunctionTimedOut
 
 
-# from selenium.webdriver.common.action_chains import ActionChains
-# from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-
-
 # import matplotlib.pyplot as plt
 
 
@@ -25,24 +20,27 @@ def start_search(query):
     opt = webdriver.ChromeOptions()
     opt.add_experimental_option('prefs', {'intl.accept_languages': 'en,en_US'})
     # opt.add_argument('headless')
-    # caps = webdriver.DesiredCapabilities.CHROME.copy()
-    # caps['acceptInsecureCerts'] = True
     driver = webdriver.Chrome("C:/Users/USER-PC/Downloads/chromedriver_win32/chromedriver.exe", options=opt)
-    # driver = webdriver.Chrome("C:/Users/USER-PC/Downloads/chromedriver_win32/chromedriver.exe")
 
-    # create action chain object
-    # action = ActionChains(driver)
-
-    # Query to obtain links
-    # query = 'Terms of Service'
     links = []  # Initiate empty list to capture final results
     # Specify number of pages on google search, each page has 10 links
     n_pages = 35
-    # Sites including with [Sample, Template, What, Frontpage, Definition, Generator] should remove
-    black_list = ['sample', 'template', 'frontpage', 'definition', 'generator', 'clauses', 'abuse',
-                  'what', 'agree', 'no', 'wikipedia', 'why', 'how', 'need', 'feed', 'not', 'click', 'spectrum']
-    whitelist = ['terms', 'conditions', 'service', 'use']
 
+    # Sites including with [...] should remove
+    blacklist = ['sample', 'template', 'frontpage', 'definition', 'generator', 'generate', 'clauses', 'abuse',
+    'what', 'different', 'difference', 'agree', 'no', 'wikipedia', 'why', 'how', 'need', 'feed', 
+    'not', 'click', 'spectrum', 'free', 'read', '?', 'tldr', 'court', 'include', 'add', 'whether',
+    'practise', 'design', 'violating', 'welcome', 'watch', 'are', 'improving', 'about', 'creating',
+    'vs.', 'versus', 'can', 'form', 'receive', 'euro', 'review', 'determining', 'to', 'in', 'apply']
+    whitelist = [
+        'terms', 
+        'condition',
+        'service', 
+        'use', 
+        'agreement',
+        'statement',
+    ]
+    
     for page in range(1, n_pages):
         url = "http://www.google.com/search?q=" + query + "&start=" + str((page - 1) * 10)
         driver.get(url)
@@ -61,15 +59,12 @@ def start_search(query):
                 'Flag': "false"
             })
 
-        # action.move_by_offset(random.randrange(1,10), random.randrange(1,10))
-        # action.perform()
-
         # try to bypass "I am not a robot".
         time.sleep(0.25)
 
     # Remove links that contain blacklist words
     for link in links:
-        for black in black_list:
+        for black in blacklist:
             # The find() method returns -1 if the value is not found.
             if link['Title'].lower().find(black) >= 0:
                 link['Flag'] = "true"
@@ -117,12 +112,12 @@ def collect_ToS_text(soup):
         'header',
         'html',
         'meta',
-        'head',
+        'head', 
         'input',
         'script',
         'style',
         'title',
-        # there may be more elements you don't want, such as "style", etc.
+    # there may be more elements you don't want, such as "style", etc.
     ]
     node = []
 
@@ -137,9 +132,8 @@ def collect_ToS_text(soup):
     df = pd.DataFrame(node)
     df_cut = df[df['Length'] > 100]
 
-    # Text preprocessing
     df_cut_revised = df_cut.copy()
-    df_cut_revised['Content'] = df_cut_revised['Content'].apply(clean_text)
+    df_cut_revised['Content'] = df_cut_revised['Content'].apply(clean_text) # Text preprocessing
     df_cut_revised['Length'] = df_cut_revised['Content'].apply(lambda x: len(x))
     final_df = df_cut_revised[df_cut_revised['Length'] > 10]
 
@@ -153,33 +147,38 @@ def collect_ToS_text(soup):
 
 def enter_link(links, driver, flag, duplicate_check, df):
     super_filename = 'whole.csv'
-
+    
     i = 0
     for link in links:
         if link['Link'].find('.pdf') >= 0 or link['Link'].find('.html') >= 0:
             continue
 
         if flag == 0:
-            duplicate_check.append(link["Link"])
+                duplicate_check.append(link["Link"])
         elif flag == 1:
-            # Confirm already accessed link
-            if link in duplicate_check:
-                continue
+                # Confirm already accessed link
+                if link["Link"] in duplicate_check:
+                    print("Accessed duplicate link. Return previous page.")
+                    continue
         try:
             print(f'Go to {link["Link"]}')
             driver.get(link["Link"])
-        except (NoSuchElementException, TimeoutException):
+        except (NoSuchElementException, TimeoutException) :
             continue
-
+        
         try:
             soup = func_timeout(300, BeautifulSoup, args=(driver.page_source, 'html.parser'))
-            # soup = BeautifulSoup(driver.page_source, 'html.parser')
         except FunctionTimedOut:
             print(f'{link["link"]} page use too many time. It is terminated.')
             continue
 
         # Save each web page, log file
-        tdf = collect_ToS_text(soup)
+        try:
+            tdf = collect_ToS_text(soup)
+        except KeyError:
+            print("KeyError happens")
+            continue
+
         path = os.getcwd() + "/data/"
         sub_filename = f'{link["Title"]}.csv'
         sub_filename = re.sub("[\/:*?\"<>|]", "", sub_filename)
@@ -188,21 +187,24 @@ def enter_link(links, driver, flag, duplicate_check, df):
 
         df = pd.concat([df, tdf])
         if i % 10 == 0:
-            df.to_csv(Path(os.getcwd() + "/" + super_filename), mode='a', index=False)
+            df.to_csv(Path(os.getcwd() + "/" + super_filename), index=False)
         i += 1
 
-        driver.back()
+        try:
+            driver.back()
+        except TimeoutException:
+            continue
 
-    df.to_csv(Path(os.getcwd() + "/" + super_filename), mode='a', index=False)  # save whole page's data
+    df.to_csv(Path(os.getcwd() + "/" + super_filename), index=False)  # save whole page's data
     print(f'Number of sentences collected: {len(df)}')
     driver.close()  # Close Chrome process
-
+    return df
 
 def main():
     duplicate_check = []
     df = pd.DataFrame(columns=['Length', 'Content'])
     links, driver = start_search('Terms of Service')
-    enter_link(links, driver, 0, duplicate_check, df)
+    df = enter_link(links, driver, 0, duplicate_check, df)
     links, driver = start_search('Terms of Conditions')
     enter_link(links, driver, 1, duplicate_check, df)
 
